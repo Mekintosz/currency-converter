91ed58aff05821bd412043761902a80e80adaf75
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2024-03-05 22:38:56 +0100

    Final refactor and styling before deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,116 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import CurrencyRow from './CurrencyRow.jsx'
-
-const apiKey = import.meta.env.VITE_CURRENCY_CONVERTER_API_KEY
-const baseApi = `http://api.exchangeratesapi.io/v1/latest?access_key=${apiKey}`
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import CurrencyRow from "./CurrencyRow.jsx";
 
 function App() {
-  const [currencyOptions, setCurrencyOptions] = useState([])
-  const [fromCurrency, setFromCurrency] = useState()
-  const [toCurrency, setToCurrency] = useState(1)
-  const [exchangeRate, setExchangeRate] = useState(1)
-  const [amount, setAmount] = useState(1)
-  const[amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+  const URL = "https://open.er-api.com/v6/latest/";
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState(0);
+  const [exchangeRate, setExchangeRate] = useState(0);
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
 
-  let toAmount, fromAmount
+  let toAmount, fromAmount;
   if (amountInFromCurrency) {
-    fromAmount = amount
-    toAmount = amount * exchangeRate
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
   } else {
-    toAmount = amount
-    fromAmount = amount / exchangeRate
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
   }
 
   useEffect(() => {
-    fetch(`https://open.er-api.com/v6/latest/EUR`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      const firstCurrency = Object.keys(data.rates)[1]
-      setCurrencyOptions([...Object.keys(data.rates)])
-      setFromCurrency(data.base_code)
-      setToCurrency(firstCurrency)
-      setExchangeRate(data.rates[firstCurrency])
-    })
-  }, [])
+    (async () => {
+      try {
+        const response = await fetch(URL + 'EUR');
+        const data = await response.json();
+        const firstCurrency = Object.keys(data.rates)[1];
+        setCurrencyOptions([...Object.keys(data.rates)]);
+        setFromCurrency(data.base_code);
+        setToCurrency(firstCurrency);
+        setExchangeRate(data.rates[firstCurrency]);
+      } catch {
+        console.log("Error occurred when fetching data");
+      }
+    })();
+  }, []);
 
   useEffect(() => {
     if (fromCurrency != null && toCurrency != null) {
-      fetch(`https://open.er-api.com/v6/latest/${fromCurrency}`)
-      .then(res => res.json())
-      .then(data => setExchangeRate(data.rates[toCurrency]))
-     
+      (async () => {
+        try {
+          const response = await fetch(URL + fromCurrency);
+          const data = await response.json();
+          setExchangeRate(data.rates[toCurrency]);
+        } catch {
+          console.log("Error occurred when fetching data");
+        }
+      })();
     }
-  }, [fromCurrency, toCurrency])
-    
-    
+  }, [fromCurrency, toCurrency]);
 
   function handleFromAmountChange(e) {
-    setAmount(e.target.value)
-    setAmountInFromCurrency(true)
+    setAmount(e.target.value);
+    setAmountInFromCurrency(true);
   }
 
   function handleToAmountChange(e) {
-    setAmount(e.target.value)
-    setAmountInFromCurrency(false)
+    setAmount(e.target.value);
+    setAmountInFromCurrency(false);
   }
 
   return (
     <>
-     <h1>Convert</h1>
-    <CurrencyRow
-      currencyOptions={currencyOptions}
-      selectedCurrency={fromCurrency}
-      onChangeCurrency={e => setFromCurrency(e.target.value)}
-      onChangeAmount={handleFromAmountChange}
-      amount={fromAmount}
-    >
-    </CurrencyRow>
-    <div className='equals'>=</div>
-    <CurrencyRow
-      currencyOptions={currencyOptions}
-      selectedCurrency={toCurrency}
-      onChangeCurrency={e => setToCurrency(e.target.value)}
-      onChangeAmount={handleToAmountChange}
-      amount={toAmount}
-    >
-    </CurrencyRow>
+      <header>
+        <img
+          className="logo"
+          src="./public/currency_exchange.svg"
+          alt="Currency Exchange Logo"
+        />
+        <p className="welcome">
+          Welcome to currency exchange rates calculator. Choose currencies you
+          want to exchange between and input amount for either. The conversion
+          amount will be calculated for you.{" "}
+          <a
+            className="wiki-link"
+            target="_blank"
+            href="https://en.wikipedia.org/wiki/ISO_4217#Active_codes_(list_one)"
+          >
+            For a list of all currencies and country codes eg. EUR, USD, CZK
+            click this sentence.
+          </a>
+        </p>
+      </header>
+      <div className="converter">
+        <h1>Convert</h1>
+        <CurrencyRow
+          currencyOptions={currencyOptions}
+          selectedCurrency={fromCurrency}
+          onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+          onChangeAmount={handleFromAmountChange}
+          amount={fromAmount}
+        />
+        <div className="equals">=</div>
+        <CurrencyRow
+          currencyOptions={currencyOptions}
+          selectedCurrency={toCurrency}
+          onChangeCurrency={(e) => setToCurrency(e.target.value)}
+          onChangeAmount={handleToAmountChange}
+          amount={toAmount}
+        />
+      </div>
+      <footer>
+        <a
+          className="link"
+          target="_blank"
+          href="https://www.exchangerate-api.com"
+        >
+          Powered By Exchange Rate API
+        </a>
+      </footer>
     </>
-   
-  )
+  );
 }
 
-export default App
+export default App;
